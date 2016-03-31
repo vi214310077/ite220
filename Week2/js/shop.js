e5fe40d3a95b8417f75acfe1c21ca451621a97e4
victoriga <victor.iga@students.stamford.edu>
2016-04-01 01:45:10 +0700

    Completed assignment Stamford Shop

--- Week2/js/shop.js
+++ Week2/js/shop.js
@@ -2,6 +2,21 @@
 var customerName = "Victor";
 var price = 10;
 var quantity = 2;
+var d = new Date().getHours();
+var sawatdee;
+
+if (d < 12){
+	sawatdee = "Good Morning"; 
+} else if (d < 23){
+	sawatdee = "Good Afternoon";
+} else {
+	sawatdee = "Good Night";
+}
+//var greetingElement = document.getElementById("greeting-time");
+//greetingElement.textContent = greeting;
+
+var sawatdeeElement = document.getElementById("time");
+sawatdeeElement.textContent = sawatdee;
 
 var customerElement = document.getElementById("customer-name");
 customerElement.textContent = customerName;
@@ -18,20 +33,15 @@ var products = [
 var prices = [10, 10, 199, 159];
 var productsText = "";
 var productsElement = document.getElementById("product-list");
-
 productsText += "<li class='list-group-item'><span class='badge'>$"+ prices[0]+"</span>" + products[0]+ "</li>";
-
 productsText += "<li class='list-group-item'><span class='badge'>$" + prices[1]+"</span>" + products[1]+ "</li>";
-
 productsText += "<li class='list-group-item'><span class='badge'>$" + prices[2]+"</span>" + products[2]+ "</li>";
-
 productsText += "<li class='list-group-item'><span class='badge'>$" + prices[3]+"</span>" + products[3]+ "</li>";
-
 productsElement.innerHTML = productsText; 
 
 var totalPrice = 0;
-
+var discount = 0.75;
 totalPrice = prices[0] +prices[1] + prices[2] + prices[3];
-
-var totalPriceElement = document.getElementById("total-price"); 
-totalPriceElement.textContent = totalPrice; 
+var discountPrice = totalPrice * discount;
+var discountPriceElement = document.getElementById("total-price"); 
+discountPriceElement.textContent = discountPrice;
